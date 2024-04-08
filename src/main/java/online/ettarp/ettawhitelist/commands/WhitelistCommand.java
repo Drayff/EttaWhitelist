65c84ee16bd2b58b185fa61881d2f2d14525bcc8
Drayff <drayf.funton@gmail.com>
2024-04-08 20:32:27 +0300

    Command fixes

--- src/main/java/online/ettarp/ettawhitelist/commands/WhitelistCommand.java
+++ src/main/java/online/ettarp/ettawhitelist/commands/WhitelistCommand.java
@@ -4,7 +4,6 @@ import online.ettarp.ettawhitelist.EttaWhitelist;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -25,23 +24,22 @@ public class WhitelistCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Player player = (Player) sender;
-
-        if(!player.hasPermission("whitelist.command")) {
-            player.sendMessage("У вас нет прав на выполнение этой команды.");
+        if(!sender.hasPermission("whitelist.command")) {
+            sender.sendMessage("У вас нет прав на выполнение этой команды.");
             return true;
         }
 
         String target;
 
         if (args == null || args.length == 0) {
-            player.sendMessage("Использование: /ewhitelist [add, remove] [season, month] <Target> <Month Count>.");
+            sender.sendMessage("Использование: /ewhitelist [add, remove] [season, month] <Target> <Month Count>.");
+            return true;
         }
 
         switch (args[0]) {
             case "add":
                 if (args.length < 2) {
-                    player.sendMessage("Использование: /ewhitelist add [season, month] <Target> <Month Count>.");
+                    sender.sendMessage("Использование: /ewhitelist add [season, month] <Target> <Month Count>.");
                     return true;
                 }
 
@@ -49,7 +47,7 @@ public class WhitelistCommand implements CommandExecutor {
 
                 if(args[1].equals("season")) {
                     if (args.length != 3) {
-                        player.sendMessage("Использование: /ewhitelist add season <Target>.");
+                        sender.sendMessage("Использование: /ewhitelist add season <Target>.");
                         return true;
                     }
 
@@ -60,7 +58,7 @@ public class WhitelistCommand implements CommandExecutor {
                             ResultSet seasonResult = statement.executeQuery();
 
                             if (seasonResult.next()) {
-                                player.sendMessage("Игрок уже есть в вайтлисте.");
+                                sender.sendMessage("Игрок уже есть в вайтлисте.");
                                 return;
                             }
 
@@ -68,14 +66,14 @@ public class WhitelistCommand implements CommandExecutor {
                             statement.setString(1, target);
                             statement.execute();
 
-                            player.sendMessage("Игрок успешно добавлен в вайтлист.");
+                            sender.sendMessage("Игрок успешно добавлен в вайтлист.");
                         } catch (SQLException e) {
                             throw new RuntimeException(e);
                         }
                     });
                 } else if(args[1].equals("month")) {
                     if (args.length != 4) {
-                        player.sendMessage("Использование: /ewhitelist add month <Target> <Month Count>.");
+                        sender.sendMessage("Использование: /ewhitelist add month <Target> <Month Count>.");
                         return true;
                     }
 
@@ -86,7 +84,7 @@ public class WhitelistCommand implements CommandExecutor {
                             ResultSet monthResult = statement.executeQuery();
 
                             if (monthResult.next()) {
-                                player.sendMessage("Игрок уже есть в вайтлисте.");
+                                sender.sendMessage("Игрок уже есть в вайтлисте.");
                                 return;
                             }
 
@@ -95,20 +93,20 @@ public class WhitelistCommand implements CommandExecutor {
                             statement.setDate(2, java.sql.Date.valueOf(LocalDate.now().plusDays(30L * Integer.parseInt(args[3]))));
                             statement.execute();
 
-                            player.sendMessage("Игрок успешно добавлен в вайтлист.");
+                            sender.sendMessage("Игрок успешно добавлен в вайтлист.");
                         } catch (SQLException e) {
                             throw new RuntimeException(e);
                         }
                     });
                 } else {
-                    player.sendMessage("Использование: /ewhitelist add [season, month] <Target> <Month Count>.");
+                    sender.sendMessage("Использование: /ewhitelist add [season, month] <Target> <Month Count>.");
                 }
 
                 break;
 
             case "remove":
                 if (args.length != 2) {
-                    player.sendMessage("Использование: /ewhitelist remove <Target>.");
+                    sender.sendMessage("Использование: /ewhitelist remove <Target>.");
                     return true;
                 }
 
@@ -139,9 +137,9 @@ public class WhitelistCommand implements CommandExecutor {
                                 plugin.getServer().getPlayer(target).kickPlayer(plugin.getConfig().getString("text.deleted-from-whitelist"));
                             }
 
-                            player.sendMessage("Игрок успешно удалён.");
+                            sender.sendMessage("Игрок успешно удалён.");
                         } else {
-                            player.sendMessage("Игрок не в вайтлисте.");
+                            sender.sendMessage("Игрок не в вайтлисте.");
                         }
                     } catch (SQLException e) {
                         throw new RuntimeException(e);
