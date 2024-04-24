2794c6d60a12a263b3df49a6b1bfa038e4cdc0ee
Tamerlan Kairbek <kairbektamerlan001@gmail.com>
2024-04-24 18:59:09 +0600

    notifying player when access is ending

--- src/main/java/online/ettarp/ettawhitelist/runnable/WhitelistChecker.java
+++ src/main/java/online/ettarp/ettawhitelist/runnable/WhitelistChecker.java
@@ -1,11 +1,20 @@
 package online.ettarp.ettawhitelist.runnable;
 
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.util.DiscordUtil;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
 import online.ettarp.ettawhitelist.EttaWhitelist;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 public class WhitelistChecker extends BukkitRunnable {
     private final EttaWhitelist plugin;
@@ -29,7 +38,32 @@ public class WhitelistChecker extends BukkitRunnable {
 
             while (resultSet.next()) {
                 Player target = plugin.getServer().getPlayer(resultSet.getString("nickname"));
+                OfflinePlayer offlineTarget = Bukkit.getOfflinePlayer(UUID.fromString(resultSet.getString("uuid")));
                 LocalDate date = resultSet.getDate("date").toLocalDate();
+
+                long daysDifference = ChronoUnit.DAYS.between(LocalDate.now(), date);
+                long absDaysDifference = Math.abs(daysDifference);
+
+                User user = DiscordUtil.getUserById(DiscordSRV.getPlugin().getAccountLinkManager().getDiscordId(UUID.fromString(resultSet.getString("uuid"))));
+                String userAsTag = String.format("<@%s>", user.getId());
+
+                if(absDaysDifference == 7) {
+                    plugin.getNotificationChannel().sendMessageEmbeds(
+                            buildEmbed(absDaysDifference, offlineTarget)
+                    ).content(userAsTag).queue();
+                }
+
+                if(absDaysDifference == 3) {
+                    plugin.getNotificationChannel().sendMessageEmbeds(
+                            buildEmbed(absDaysDifference, offlineTarget)
+                    ).content(userAsTag).queue();
+                }
+
+                if(absDaysDifference == 1) {
+                    plugin.getNotificationChannel().sendMessageEmbeds(
+                            buildEmbed(absDaysDifference, offlineTarget)
+                    ).content(userAsTag).queue();
+                }
 
                 if(LocalDate.now().isAfter(date)) {
                     if(target != null) {
@@ -44,5 +78,26 @@ public class WhitelistChecker extends BukkitRunnable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public MessageEmbed buildEmbed(long days, OfflinePlayer player) {
+        EmbedBuilder builder = new EmbedBuilder();
+
+        String daysAsText;
+        if(days == 7) {
+            daysAsText = "дней";
+        } else if (days == 3) {
+            daysAsText = "дня";
+        } else {
+            daysAsText = "день";
+        }
+        String warn = String.format("Ваша проходка закончится через %d %s", days, daysAsText);
+        String buy = "Оплатите продление проходки на сайте:\nhttps://shop.ettarp.com/";
+        String urlSkin = String.format("http://cravatar.eu/helmhead/%s.png", player.getName());
+
+        builder.setAuthor(warn, urlSkin, urlSkin);
+        builder.setDescription(buy);
+
+        return builder.build();
     }
 }
