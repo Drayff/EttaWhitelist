2ce7801161113b793bbe0c3c092a37392ee384e6
Tamerlan Kairbek <kairbektamerlan001@gmail.com>
2024-05-21 03:14:34 +0500

    инициализация нового менеджера и запуск таска о сохранении NotifiedUser

--- src/main/java/online/ettarp/ettawhitelist/EttaWhitelist.java
+++ src/main/java/online/ettarp/ettawhitelist/EttaWhitelist.java
@@ -1,15 +1,16 @@
 package online.ettarp.ettawhitelist;
 
-import github.scarsz.discordsrv.DiscordSRV;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 import github.scarsz.discordsrv.util.DiscordUtil;
 import online.ettarp.ettawhitelist.commands.WhitelistCommand;
 import online.ettarp.ettawhitelist.commands.WhitelistCompleter;
 import online.ettarp.ettawhitelist.db.DBHandler;
+import online.ettarp.ettawhitelist.db.NotifiedUserManager;
 import online.ettarp.ettawhitelist.listeners.PreLoginEvent;
 import online.ettarp.ettawhitelist.runnable.WhitelistChecker;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -17,6 +18,8 @@ import java.sql.SQLException;
 public final class EttaWhitelist extends JavaPlugin {
     DBHandler handler;
     TextChannel notificationChannel;
+
+    NotifiedUserManager nUM;
 
     @Override
     public void onEnable() {
@@ -36,12 +39,32 @@ public final class EttaWhitelist extends JavaPlugin {
             }
         });
 
-        new WhitelistChecker(this).runTaskTimerAsynchronously(this, 0, 60*60*20);
+        new WhitelistChecker(this).runTaskTimerAsynchronously(this, 20 * 60 * 10, 20 * 60 * 60 * 4);
+        nUM = new NotifiedUserManager(this);
+
+        this.getServer().getScheduler().runTaskAsynchronously(this, () -> {
+            try {
+                nUM.loadNotifiedUsers();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        this.getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+            try {
+                nUM.saveNotifiedUsers();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }, 20 * 60L, 20 * 60 * 60L);
 
         getCommand("ewhitelist").setExecutor(new WhitelistCommand(this));
         getCommand("ewhitelist").setTabCompleter(new WhitelistCompleter());
 
         getServer().getPluginManager().registerEvents(new PreLoginEvent(this), this);
+    }
+
+    public NotifiedUserManager getNUM() {
+        return nUM;
     }
 
     public TextChannel getNotificationChannel() {
